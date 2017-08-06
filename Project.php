15e59d448595005dd1e2f4413a28d49cb899d490
paris <suppersoft@gmail.com>
2017-08-06 19:43:45 +0800

    bugfix

--- Project.php
+++ Project.php
@@ -7,6 +7,8 @@ use Composer\Script\Event;
 class Project{
     public static function postInstall(Event $e){
         echo "POST INSTALL\r\n";
-        print_r($e);
+        foreach($e as $key => $value){
+            echo "$key => $value\r\n";
+        }
     }
 }
