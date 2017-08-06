8745e0b2c2d00dbc5022b97d6ca8b0c9d7356347
paris <suppersoft@gmail.com>
2017-08-06 23:13:44 +0800

    apache rewrite

--- Project.php
+++ Project.php
@@ -6,10 +6,27 @@ use Composer\Script\Event;
 
 class Project{
     public static function postInstall(Event $e){
-        echo "POST INSTALL\r\n";
-        foreach($e as $key => $value){
-            echo "    $key => $value\r\n";
+        // $composer = $e->getComposer();
+        // vendor-dir
+        // $config = $composer->getConfig();
+        // $package = $composer->getPackage();
+        static::rewriteServer();
+    }
+
+    protected static function rewriteServer(){
+        $apacheRewrite = "<IfModule mod_rewrite.c>\r\n".
+            "RewriteEngine on\r\n".
+            // "RewriteCond %{HTTP_HOST} ^(www.)?wp.com$\r\n".
+            "RewriteCond %{REQUEST_URI} !^/wp/\r\n".
+            "RewriteCond %{REQUEST_FILENAME} !-f\r\n".
+            "RewriteCond %{REQUEST_FILENAME} !-d\r\n".
+            "RewriteRule ^(.*)$ /wp/$1 [L]\r\n".
+            // "RewriteCond %{HTTP_HOST} ^(www.)?wp.com$\r\n".
+            "RewriteRule ^(/)?$ wp/index.php [L]\r\n".
+            "</IfModule>";
+        $filename = getcwd() . '/.htaccess';
+        if(!file_exists($filename)){
+            file_put_contents($filename, $apacheRewrite);
         }
-        echo "END\r\n";
     }
 }
